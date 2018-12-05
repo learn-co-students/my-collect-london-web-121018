4c266eb7093dc2ef1d4dfd8c3b24ee9d76d06e16
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-05 14:56:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0 
+    while i < array.length
+    yield array[i]
+    
+    if array[i].include?(" ")
+      new_array << array[i].split(" ").first
+    
+    elsif array[i].downcase
+     new_array << array[i].upcase
+    end
+    i += 1 
+    end
+    new_array
+  else
+    return "This block should not run!"
+  end
+end
+
+
 
 
