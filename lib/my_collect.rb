f63bda273fe58bbf572a911d047da51c22e44b68
I_AM Elaine <elaine.onb@gmail.com>
2018-12-08 08:44:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(arr)
+  if block_given?
+     new_arr = []
+     int = 0  
+     while int < arr.length 
+       new_arr << yield(arr[int])
+       int += 1 
+     end
+     return new_arr
+  end
+end
+       
+       
+    
 
